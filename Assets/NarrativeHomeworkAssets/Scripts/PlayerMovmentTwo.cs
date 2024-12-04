43300692b1aa49c89f9da8b8a397a0775d349d5e
ZhengCynthia <cynthiazheng1124@gmail.com>
2024-12-04 18:14:51 -0500

    MainMenu

---
+++ Assets/NarrativeHomeworkAssets/Scripts/PlayerMovmentTwo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovmentTwo : MonoBehaviour
+{
+    private Rigidbody2D _rigRigidbody2D;
+    private PlayerDialogue _playerDialogue;
+    private float _xVelocity = 0f;
+    private float _yVelocity = 0f;
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _xVelocity = Input.GetAxis("Horizontal");
+        _yVelocity = Input.GetAxis(Structs.Input.vertical);
+
+        
+        _rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed; 
+    }
+}

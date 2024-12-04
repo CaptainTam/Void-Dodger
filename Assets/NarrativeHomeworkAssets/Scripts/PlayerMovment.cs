9ebd6071b1d88952af7c299390a847d7410c84d7
CaptainTam <tammyzheng1101@gmail.com>
2024-12-04 18:12:38 -0500

    12/4/24

--- Assets/NarrativeHomeworkAssets/Scripts/PlayerMovment.cs
+++ Assets/NarrativeHomeworkAssets/Scripts/PlayerMovment.cs
@@ -10,10 +10,13 @@ public class PlayerMovment : MonoBehaviour
     private float _xVelocity = 0f;
     private float _yVelocity = 0f;
     public float speed = 3;
+    public float walkTime;
+    private float walkTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        walkTimer = 0;
         _rigRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -24,7 +27,19 @@ public class PlayerMovment : MonoBehaviour
         _xVelocity = Input.GetAxis("Horizontal");
         _yVelocity = Input.GetAxis("Vertical");
 
+
+        if(_xVelocity == -1)
+        {
+
+            if(walkTimer <= 0)
+            {
+                transform.position += (Vector3) Vector2.left * 1.2f;
+                walkTimer = walkTime;
+            }
+
+           walkTimer -= Time.deltaTime;
+        }
         
-        _rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed; 
+        //_rigRigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity) * speed; 
     }
 }

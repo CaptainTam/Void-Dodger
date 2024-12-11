b8723bb831bbe7423fb5bb6cc5251f4ca8a57316
CaptainTam <tammyzheng1101@gmail.com>
2024-12-11 17:53:58 -0500

    change

--- Assets/PixelQuestAssets/Scripts/MainMenu.cs
+++ Assets/PixelQuestAssets/Scripts/MainMenu.cs
@@ -4,11 +4,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string startLevel = "TestLevel"; // Level we are going to start the game from
+    public string menuScene = "Main Menu";
+
 
     // Used by the UI button to load us into the given level 
     public void StartButton()
     {
         SceneManager.LoadScene(startLevel);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuScene);
     }
 
 
